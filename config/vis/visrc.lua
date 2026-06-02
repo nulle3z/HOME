46353c9abb898c6ac95bc4d8f7adccd78db394ee
eason <esonzhou0801@163.com>
2026-06-02 16:29:51 +0800

    16:29 06.02 update by script.

---
+++ config/vis/visrc.lua
@@ -0,0 +1,21 @@
+-- 基础显示
+vis.options.number = true
+-- vis.options.relativenumber = true
+vis.options.syntax = true
+vis.options.show_tabs = true
+
+-- 缩进行为
+vis.options.expandtab = true
+vis.options.tabwidth = 4
+vis.options.shiftwidth = 4
+
+-- 搜索增强
+vis.options.ignorecase = true
+vis.options.smartcase = true
+
+-- 主题
+vis.options.theme = 'default-16'
+
+-- vis:map(vis.modes.NORMAL, '<Space>', '<Nop>')
+-- vis.leader = ' '
+
